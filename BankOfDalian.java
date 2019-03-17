8fe3edf49f83dd6b441eceb05ec3e1f8e5657a54
waduhecks <45762082+waduhecks@users.noreply.github.com>
2019-03-17 16:14:36 +0800

    Create BankOfDalian.java

---
+++ BankOfDalian.java
@@ -0,0 +1,16 @@
+public class BankOfDalian extends Bank{
+
+    double year;
+
+    @Override
+    public double computeInterest() {
+
+        super.year = (int)year;
+        double day_in_year = year - (int)year;
+        int day = (int)(day_in_year * 1000);
+        interest = super.computeInterest() + day * 0.00012 * savedMoney;
+
+        System.out.println("在大连银行存钱为: " + savedMoney + "元" + " 利息为:" + interest + "元");
+        return interest;
+    }
+}
